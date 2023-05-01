42c7a2d85f1dd233761f2f512fd7001397383a5c
Daniel Møller <57629796+Danielm0402@users.noreply.github.com>
2023-05-01 10:51:41 +0200

    createProduct
    Made route to receive values from addProduct form

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { getDataFromFirestore } from "./assets/js/Firestore.js";
 
 import express from "express";
+import bodyParser from "body-parser";
 const app = express();
 
+//middleware
 app.use(express.static("assets"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // 1. templating
 app.set("view engine", "pug");
@@ -22,9 +25,23 @@ app.get("/", async (req, res) => {
   res.render("index", { varer: varer });
 });
 
-app.get("/form", (req, res) => {
+app.get("/createProduct", (req, res) => {
   res.render("addProduct");
 });
+
+app.post("/productCreated", (req, res) => {
+  const productName = req.body.inputName
+  const productID = req.body.inputProductID
+  const amount = req.body.inputAmount
+  const unit = req.body.dropdownUnit
+
+  console.log(productName)
+  console.log(productID)
+  console.log(amount)
+  console.log(unit)
+
+  res.redirect("/createProduct")
+})
 
 app.listen(4000);
 console.log("listening on port 4000");
