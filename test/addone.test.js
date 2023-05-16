566d3ff5d0b57fc4689125a6adacc40f1e5d1371
Rasmus K <96525075+RasmusKvejborg@users.noreply.github.com>
2023-05-16 09:55:58 +0200

    test1 virker

--- test/addone.test.js
+++ test/addone.test.js
@@ -2,33 +2,36 @@ import { describe } from "mocha";
 import { updateAmountToProduct } from "../database/Firestore.js";
 
 import { assert } from "chai";
+import Controller from "../controllers/controller.js";
 
-let x = async () => {
-  describe("When incrementing lagertal", () => {
-    it("Should return correct result", async () => {
-      //
-      const currentNumber = updateAmountToProduct(0, 50); // 0 is amount, 50 is productNumber
+let controller = new Controller();
 
-      // const result = await updateAmountToProduct(1, 50);
+describe("When incrementing lagertal", () => {
+  it("Should return correct result", async () => {
+    //
 
-      console.log("result ", currentNumber);
+    const productID = "1017060812";
 
-      const expected = currentNumber + 1;
+    const current = await controller.adjustProductAmount(productID, 0); // ændrer med 0 for at få nuværende værdi
 
-      assert.equal(1, 1);
-    });
+    const amountToChange = 7;
 
-    //   it("has a big number", () => {
-    //     const result = add(8000, 1000);
-    //     assert.equal(result, 9000);
-    //   });
+    const expected = current + amountToChange;
 
-    //   // der findes også assert.oneOf og assert.isString osv osv...
+    const actual = await controller.adjustProductAmount(
+      productID,
+      amountToChange
+    );
 
-    //   it("Should return another correct result", () => {
-    //     const result = add(1, 3);
-    //     assert.equal(result, 4);
-    //   });
+    console.log(
+      "current: ",
+      current,
+      "\namount to change: ",
+      amountToChange,
+      "\nactual: ",
+      actual
+    );
+
+    assert.equal(actual, expected);
   });
-};
-x();
+});
